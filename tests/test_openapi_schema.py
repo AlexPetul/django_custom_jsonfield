4967eb1e19e3d6125f939029b581600c60033bbe
Alex Petul <alekspetul@gmail.com>
2024-05-22 22:47:29 +0400

    Add support for "const" keyword in JSON schema (#1)
    * Add support for "const" keyword in JSON schema
    
    * Add tests for const list and dict

--- tests/test_openapi_schema.py
+++ tests/test_openapi_schema.py
@@ -1,3 +1,4 @@
+from typing import Any
 from unittest.mock import Mock
 
 import pytest
@@ -48,6 +49,28 @@ def test_map_serializer_field_ok(schema: dict):
     extension = CustomJSONFieldSerializerExtension(json_field)
     data = extension.map_serializer_field(Mock(), "response")
     assert data == schema
+
+
+@pytest.mark.parametrize(
+    "schema,expected",
+    [
+        ({"const": 10}, {"enum": [10]}),  # integer
+        ({"const": 10.00}, {"enum": [10.00]}),  # float
+        ({"const": 10.00}, {"enum": [10.00]}),  # bytes
+        ({"const": "string"}, {"enum": ["string"]}),  # string
+        ({"const": True}, {"enum": [True]}),  # bool
+        ({"const": None}, None),  # none
+        ({"const": {"k": "v", "k2": 10}}, {"enum": [{"k": "v", "k2": 10}]}),  # dict
+        ({"const": [10, 20]}, {"enum": [[10, 20]]}),  # list
+    ],
+)
+def test_map_serializer_field_const(schema: dict, expected: Any):
+    """Test basic types declared as const in JSON schema."""
+
+    json_field = CustomJSONField(schema=schema)
+    extension = CustomJSONFieldSerializerExtension(json_field)
+    data = extension.map_serializer_field(Mock(), "response")
+    assert data == expected
 
 
 @pytest.mark.parametrize(
